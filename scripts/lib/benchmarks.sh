23107afa539de5cf39c080f8feac5752bb30b149
Deyaaeldeen Almahallawi <dalmahal@indiana.edu>
2018-10-01 17:09:03 -0400

    increase the runtime for blackscholes

--- scripts/lib/benchmarks.sh
+++ scripts/lib/benchmarks.sh
@@ -2,6 +2,6 @@
 
 declare -a BENCHMARKS=(quicksort fft blackscholes matmult n_body tak ray array)
 
-declare -a BENCHMARKS_ARGS_LATTICE=("in_descend1000.txt" "slow.txt" "in_4K.txt" "400.txt" "slow.txt" "slow.txt" "empty.txt" "slow.txt")
+declare -a BENCHMARKS_ARGS_LATTICE=("in_descend1000.txt" "slow.txt" "in_16K.txt" "400.txt" "slow.txt" "slow.txt" "empty.txt" "slow.txt")
 
 declare -a BENCHMARKS_ARGS_EXTREMES=("in_descend10000.txt" "slow.txt" "in_64K.txt" "400.txt" "slow.txt" "slow.txt" "empty.txt")

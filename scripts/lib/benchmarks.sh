4c2408275de89307c044a9059776d195795899c1
Deyaaeldeen Almahallawi <dalmahal@indiana.edu>
2020-02-26 21:29:38 -0500

    adding slower input to fft

--- scripts/lib/benchmarks.sh
+++ scripts/lib/benchmarks.sh
@@ -8,4 +8,4 @@ declare -a BENCHMARKS_ARGS_PARTIAL_FINE=("trivial.txt" "medium1.txt" "in_4K.txt"
 
 declare -a BENCHMARKS_ARGS_TRIVIAL=("trivial.txt" "fast.txt" "in_4K.txt" "fast.txt" "empty.txt" "in_descend1000.txt" "200.txt" "slow.txt" "slow.txt")
 
-declare -a BENCHMARKS_ARGS_EXTERNAL=("slow.txt" "slow.txt" "in_16K.txt" "slow.txt" "empty.txt" "in_descend10000.txt" "400.txt" "slow.txt" "slow.txt")
+declare -a BENCHMARKS_ARGS_EXTERNAL=("slow.txt" "slow2.txt" "in_16K.txt" "slow.txt" "slow.txt" "in_descend10000.txt" "400.txt" "slow.txt" "slow.txt")
